e5f9df73740489ba7de6681684f34a454ec7df3b
Jeferson Barros <im.jbalves@gmail.com>
2017-02-18 15:11:51 -0400

    Initial Project

---
+++ Assets/Scripts/FlappyBirdController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlappyBirdController : MonoBehaviour {
+
+	[Header("Velocity")]
+	public float flappyVelocity;
+
+	[Header("Animation")]
+	private Animator animator;
+	private Rigidbody2D rigidBody2d;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+		rigidBody2d = GetComponent<Rigidbody2D> ();
+
+		//Convertendo coordenadas de tela para o Mundo
+		Vector3 startPos = Camera.main.ViewportToWorldPoint (new Vector3 (0.2f, 0.8f));
+
+		startPos.z = 0f;
+		transform.position = startPos;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			FlappyWings ();
+		}
+
+		Vector3 angles = transform.eulerAngles;
+		angles.z = Mathf.Clamp(rigidBody2d.velocity.y * 4f, -90f, 45f);
+		transform.eulerAngles = angles;
+	}
+
+	//Faz o Flappy Bird bater asas
+	void FlappyWings() {
+		//Anima o Flappy Bird
+		animator.SetTrigger ("Flappy");
+		//Movimenta o Flappy Bird
+		rigidBody2d.velocity = Vector2.up * flappyVelocity;
+	}
+}
---
+++ Assets/Scripts/PipesManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipesManager : MonoBehaviour {
+
+	[Header("References")]
+	public PipeScroll pipePrefab;
+
+	[Header("Pipes")]
+	public int maxPipes;
+	public float pipesOffset;
+
+	private Transform[] pipes;
+	private int firstPipeIndex;
+
+	// Use this for initialization
+	void Start () {
+		
+		pipes = new Transform[maxPipes];
+
+		Vector3 pipePosition;
+
+		for (int i = 0; i < maxPipes ; i++) {
+			//Encontra a posição de cada pipe
+			pipePosition = Camera.main.ViewportToWorldPoint (
+				new Vector3 (1f + pipesOffset * i, 0f, 0f)
+			);
+			pipePosition.z = 0f;
+
+			//Criar cada pipe
+			pipes [i] = Instantiate(pipePrefab.transform) as Transform;
+			pipes [i].parent = transform;
+			pipes [i].position = pipePosition;
+		}
+		firstPipeIndex = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
