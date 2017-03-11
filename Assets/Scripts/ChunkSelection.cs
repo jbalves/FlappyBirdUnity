a32df14185286d2b9b97d11e14bd137448b8a3ee
Jeferson Barros <soldier.jhonson@gmail.com>
2017-03-11 15:31:58 -0400

    update

---
+++ Assets/Scripts/ChunkSelection.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public static class ArrayExtensions{
+	public static T GetRandom<T>(this T[] array) {
+		return array [Random.Range (0, array.Length)];
+	}
+}
+
+public class ChunkSelection : MonoBehaviour {
+
+	[Header("Prefab Reference")]
+	public NestedPrefab[] chunks;
+
+	//Componentes do mesmo objeto (Escolhido aleatoriamente)
+	public NestedPrefab lastSelectedChunk;
+	public ScrollingObject currentScrollObject;
+
+	public void SelectChunk() {
+		if (currentScrollObject == null) 
+			currentScrollObject = GetComponent<ScrollingObject> ();
+			
+		lastSelectedChunk = chunks.GetRandom ();
+		//Pega o tamanho do chunck atual
+		currentScrollObject.size = lastSelectedChunk.GetComponent<ScrollingObject> ().size;
+	}
+
+	public Transform GenerateChunck() {
+		return Instantiate (lastSelectedChunk.transform) as Transform;
+	}
+}
--- Assets/Scripts/ShareScreenshot.cs
+++ Assets/Scripts/ShareScreenshot.cs
@@ -1,19 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class ShareScreenshot : MonoBehaviour {
 
 	public void CaptureAndShareScreenshot () {
 		CaptureScreenshot ();
-		NativeShareScreenshot ();
+		//NativeShareScreenshot ();
+		ShareScreenshotImage();
 	}
 
 	public void CaptureScreenshot() {
 		Texture2D tex = new Texture2D (Screen.width, Screen.height);
 		tex.ReadPixels (new Rect(0,0,Screen.width,Screen.height),0,0);
+
+		//salva o texture em um arquivo JPG
 		byte[] bytes = tex.EncodeToJPG ();
 
+		string filepath = getAndroidExternalStoragePath () + "/temporary_file.jpg";
+
+		if (File.Exists (filepath)) {
+			File.Delete (filepath);
+		}
+
+		File.WriteAllBytes (filepath, bytes); //salva imagem
+	}
+
+	public void ShareScreenshotImage() {
+		AndroidJavaClass intentClass = new AndroidJavaClass ("android.content.Intent");
+		AndroidJavaObject intentObject = new AndroidJavaObject ("android.content.Intent");
+
+		intentObject.Call<AndroidJavaObject> ("setAction", intentClass.GetStatic<string> ("ACTION_SEND"));
+
+		AndroidJavaClass uriClass = new AndroidJavaClass ("android.net.Uri");
+		AndroidJavaObject fileObject 
+			= new AndroidJavaObject ("java.io.File", getAndroidExternalStoragePath () + "/temporary_file.jpg");
+
+		AndroidJavaObject uriObject 
+			= uriClass.CallStatic<AndroidJavaObject> ("fromFile", fileObject);
+		intentObject.CallStatic<AndroidJavaObject> ("putExtra",
+			intentClass.GetStatic<string> ("EXTRA_STREAM"),
+			uriObject);
+
+		AndroidJavaObject curActivity = GetCurrentActivity ();
+		curActivity.Call ("startActivity", intentObject);
+		
 	}
 
 	public void NativeShareScreenshot() {
@@ -23,13 +55,13 @@ public class ShareScreenshot : MonoBehaviour {
 
 	public AndroidJavaObject GetCurrentActivity() {
 		AndroidJavaClass jc = new AndroidJavaClass ("com.unity3d.player.UnityPlayer");
-		return jc.CallStatic<AndroidJavaObject> ("currentActivity");
+		return jc.GetStatic<AndroidJavaObject> ("currentActivity");
 	}
 
 	public string getAndroidExternalStoragePath() {
 		string path = " ";
 
-		AndroidJavaClass jc = new AndroidJavaClass ("Environment");
+		AndroidJavaClass jc = new AndroidJavaClass ("android.os.Environment");
 
 		path = jc.CallStatic<AndroidJavaObject> ("getExternalStorageDirectory").Call<string> ("getAbsolutePath");
 
